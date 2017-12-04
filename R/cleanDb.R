282abd72af067b437f1f971a0ee2866089904200
patrickbarks <patrick.barks@gmail.com>
2017-12-04 17:27:36 +0100

    Add checks for NAs, ergodicity, primitivty, etc.

--- R/cleanDb.R
+++ R/cleanDb.R
@@ -27,12 +27,25 @@
 #' @references %% references
 #' @keywords ~kwd1 ~kwd2
 #' @examples
-#' 
-#' 
+
+
 CleanDB <- function(DB) {
+  DB$metadata$index <- 1:nrow(DB$metadata)
+
+  DB$metadata$check_NA_A <- sapply(DB$mat, function(x) any(is.na(x$matA)))
+  DB$metadata$check_NA_U <- sapply(DB$mat, function(x) any(is.na(x$matU)))
+  DB$metadata$check_NA_F <- sapply(DB$mat, function(x) any(is.na(x$matF)))
+  DB$metadata$check_NA_C <- sapply(DB$mat, function(x) any(is.na(x$matC)))
   
+  DB$metadata$check_colsums_U <- sapply(DB$mat, function(x) any(colSums(x$matU, na.rm = T) > 1))
   
-   <- sapply(DB$mat, function(x) any(is.na(x$matA)))
+  DB_sub <- subsetDB(DB, check_NA_A == F)
+  DB_sub$metadata$check_ergodic <- sapply(DB_sub$mat, function(x) popdemo::isErgodic(x$matA))
+  DB_sub$metadata$check_primitive <- sapply(DB_sub$mat, function(x) popdemo::isPrimitive(x$matA))
+  DB_sub$metadata$check_irreducible <- sapply(DB_sub$mat, function(x) popdemo::isIrreducible(x$matA))
   
+  DB_sub$metadata <- subset(DB_sub$metadata, select = c('index', 'check_ergodic', 'check_primitive', 'check_irreducible'))
   
+  DB$metadata <- merge(DB$metadata, DB_sub$metadata, by = 'index', all.x = T)
+  DB$metadata <- subset(DB$metadata, select = -index)
 }
