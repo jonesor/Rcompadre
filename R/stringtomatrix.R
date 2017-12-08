ebea8789250927b20f98b66b2feb2700d5f4f142
patrickbarks <patrick.barks@gmail.com>
2017-12-08 14:42:10 +0100

    Added functionality to throw error if matrix is not square, and also to convert character "NA" to R type NA -- which may prevent a warning message from the call to as.numeric(). Also edited documentation.

--- R/stringtomatrix.R
+++ R/stringtomatrix.R
@@ -1,24 +1,40 @@
-#' A function to convert a matrix from the character string format to a matrix
+#' Converts a matrix in character string format to numeric matrix format
 #' 
-#' The function converts a matrix from the character string format to class matrix.
+#' This function converts a matrix in character string format (e.g. "[0.2 0.3
+#' 0.1 0]") to a square numeric matrix.
 #' 
-#' 
-#' %% ~~ If necessary, more details than the description above ~~
-#' 
-#' @param A A matrix presented in the form of a string, with values separated by spaces, and bounded by square brackets.
-#' @return A `matrix`.
+#' @param A A square matrix in the form of a string, begining with an open
+#'   square bracket and ending with a closed square bracket, with individual
+#'   matrix cell entries separated by a space.
+#' @return A square numeric \code{matrix}.
 #' @author Owen R. Jones <jones@@biology.sdu.dk>
 #' @seealso convert2flat
 #' @examples
+#' x1 <- "[3.3 5.2 6.1 0.1 NA 0.3 0.2 0.4 0.1]"
+#' stringtomatrix(x1)
 #' 
-#' x <- "[0.46145 0.153 0.33615 NA 0.5238 0.59225 NA 0.2118 0.33615 0.24765 0.5238 0.59225 0.0705 0.0633 0.10815 0.0827 0 0 0.0267 0.0947 0.03155 0.15405 0 0 0.06295 0.19195 0.3909 0.24355 0 0 0.02305 0.281 0.1321 0.35145 0 0]"
-#' stringtomatrix(x)
+#' x2 <- "[0 0 0 0.42 0.65 0 0 0 0 0.52 0 0 0 0 0.2 0]"
+#' stringtomatrix(x2)
 #' 
-#' @export stringtomatrix
-
+#' \dontrun{
+#' # non-square matrix
+#' x3 <- "[0.42 0.52 0.15 0.23 0.14]"
+#' stringtomatrix(x3)
+#' }
+#' @export
 stringtomatrix <- function(A) {
   A <- gsub(pattern = "\\[|\\]", "", A)
   A <- gsub(pattern = ";", " ", A)
   A <- strsplit(x = A, split = " |`NA`")[[1]]
-  matrix(as.numeric(A), nrow = sqrt(length(A)), byrow = TRUE)
+  A[which(A == 'NA')] <- NA
+  
+  # check whether A will form square matrix
+  matDim <- sqrt(length(A))
+  if(abs(as.integer(matDim) - matDim) > 0.00000001) {
+    stop("This matrix is not square")
+  }
+  
+  # convert to matrix and return
+  mat <- matrix(as.numeric(A), nrow = sqrt(length(A)), byrow = TRUE)
+  return(mat)
 }
