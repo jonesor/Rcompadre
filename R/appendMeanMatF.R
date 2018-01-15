aea9739cc158f4f68cb6a861fa3adde7f50b4ba3
wpetry <wpetry@users.noreply.github.com>
2018-01-15 16:41:15 +0100

    progress towards S4 addressing; relates to #15

--- R/appendMeanMatF.R
+++ R/appendMeanMatF.R
@@ -10,7 +10,8 @@
 #' YearPublication, DOI.ISBN, SpeciesAuthor, MatrixPopulation, and
 #' MatrixDimension.
 #' 
-#' @param db A COM(P)ADRE database object.
+#' @param db A COM(P)ADRE database object. Database versions <=4.0.1 will be coerced
+#'  from class 'list'.
 #' @return Returns db, but with an extra entry in slot `mat` which contains the
 #'   mean fecundity matrix associated with a given row of the database, or NA if
 #'   there are no mean matrices within the db from the relevant population.
@@ -32,28 +33,34 @@
 #' @export
 #' @importFrom rlang .data
 appendMeanMatF <- function(db) {
+  # convert legacy versions of COM(P)ADRE from class 'list' to 'CompadreData'
+  if (class(db) == "list"){
+    if (as.numeric(gsub("\\.", "", sub("(\\s.*$)", "", db$version$Version))) <= 401){
+      db <- as(db, "CompadreData")
+    }
+  }
   
   # create a unique identifier for each population in the database
-  db$metadata$PopId <- as.numeric(as.factor(paste(
-    db$metadata$Authors,
-    db$metadata$YearPublication,
-    db$metadata$DOI.ISBN,
-    db$metadata$SpeciesAuthor,
-    db$metadata$MatrixPopulation,
-    db$metadata$MatrixDimension
+  db@metadata$PopId <- as.numeric(as.factor(paste(
+    db@metadata$Authors,
+    db@metadata$YearPublication,
+    db@metadata$DOI.ISBN,
+    db@metadata$SpeciesAuthor,
+    db@metadata$MatrixPopulation,
+    db@metadata$MatrixDimension
   )))
   
   # subset database to only mean matrices that are divided,
   # and create unique row ID
-  ssdb_mean <- subsetDB(db, .data$MatrixComposite == "Mean" &
-                          .data$MatrixSplit == "Divided")
+  ssdb_mean <- subsetDB(db, MatrixComposite == "Mean" &
+                          MatrixSplit == "Divided")
   
-  ssdb_mean$metadata$RowId <- seq_len(nrow(ssdb_mean$metadata)) 
+  ssdb_mean@metadata$RowId <- seq_len(nrow(ssdb_mean@metadata)) 
   
   # function to return a mean mean matF given PopId
   meanMatF <- function(PopIdFocal) {
-    RowId <- subset(ssdb_mean$metadata, .data$PopId == PopIdFocal)$RowId
-    meanMatFs <- lapply(RowId, function(y) ssdb_mean$mat[[y]]$matF)
+    RowId <- subset(ssdb_mean@metadata, PopId == PopIdFocal)$RowId
+    meanMatFs <- lapply(RowId, function(y) ssdb_mean@mat[[y]]@matF)
     
     if (length(meanMatFs) == 0) {        # if no meanMatF for given PopId
       return(NA)
@@ -65,20 +72,20 @@ appendMeanMatF <- function(db) {
   }
   
   # create vector of unique PopIds, and list of corresponding meanMatFs
-  unique_study_pop <- sort(unique(db$metadata$PopId))
+  unique_study_pop <- sort(unique(db@metadata$PopId))
   unique_mean_mat_F <- lapply(unique_study_pop, meanMatF)
   
   # function to append meanMatF to slot mat, given row number of db
   appendMeanMatF <- function(i) {
-    PopId <- db$metadata$PopId[i]
+    PopId <- db@metadata$PopId[i]
     index_mean_mat_F <- which(unique_study_pop == PopId)
-    out <- db$mat[[i]]
-    out$meanMatF <- unique_mean_mat_F[[index_mean_mat_F]]
+    out <- db@mat[[i]]
+    out@matFmean <- unique_mean_mat_F[[index_mean_mat_F]]  ## FAILS here because not valid slot name
     return(out)
   }
   
   # append meanMatFs for each row of database
-  db$mat <- lapply(1:nrow(db$metadata), appendMeanMatF)
+  db@mat <- lapply(1:nrow(db@metadata), appendMeanMatF)
   
   return(db)
 }
