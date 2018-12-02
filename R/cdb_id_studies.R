f02626eccbe25c0c20554286768bc8b72897ce58
patrickbarks <patrick.barks@gmail.com>
2018-12-02 11:10:21 +0100

    rebrand cdb_vec_studies -> cdb_id_studies

---
+++ R/cdb_id_studies.R
@@ -0,0 +1,35 @@
+#' Create a vector of unique study identifiers from a COM(P)ADRE database
+#'
+#' Creates a vector of integer study identifiers corresponding to the rows of a
+#' CompadreDB object, based on unique combinations of the columns 'Authors',
+#' 'Journal', 'YearPublication', and 'DOI.ISBN' (or optionally, a different set
+#' of columns supplied by the user).
+#'
+#' @param cdb A CompadreDB object
+#' @param columns A vector of column names from which unique combinations should
+#'   be identified. Defaults to \code{c("Authors", "Journal", "YearPublication",
+#'   "DOI.ISBN")}.
+#' 
+#' @return Vector of integer study identifiers corresponding to the rows of
+#'   \code{cdb}, based on unique combinations of the elements in \code{columns}.
+#' 
+#' @author Patrick Barks <patrick.barks@@gmail.com>
+#' 
+#' @details
+#' Identifiers are assigned by pasting together the relevant columns, assigning
+#' factor levels based on alphabetical order, and then converting the factor
+#' levels to integers, as in \code{as.integer(as.factor(paste(cdb[,columns],
+#' collapse = "")))}.
+#' 
+#' @seealso \link{cdb_id}
+#' 
+#' @examples
+#' Compadre$StudyID <- cdb_id_studies(Compadre)
+#' 
+#' @export cdb_id_studies
+cdb_id_studies <- function(cdb, columns = c("Authors", "Journal",
+                                            "YearPublication", "DOI.ISBN")) {
+
+  cdb_id(cdb, columns)
+}
+
---
+++ tests/testthat/test-cdb_id_studies.R
@@ -0,0 +1,21 @@
+context("cdb_id_studies")
+
+test_that("cdb_id_studies works correctly", {
+  
+  s1 <- cdb_id_studies(Compadre)
+  expect_is(s1, "integer")
+  expect_length(s1, nrow(Compadre@data))
+  
+  s2 <- cdb_id_studies(Compadre, c("Family", "MatrixPopulation"))
+  expect_is(s2, "integer")
+  expect_length(s2, nrow(Compadre@data))
+  expect_true(!all(s1 == s2))
+})
+
+
+test_that("cdb_id_studies warns and fails gracefully", {
+  
+  expect_error(cdb_id_studies(Compadre@data))
+  expect_error(cdb_id_studies(Compadre, c("SpeciesAuthor", "blah")))
+})
+
