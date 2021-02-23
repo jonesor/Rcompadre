42361d04ac3e651c3e45647ea539dc0aadd25a29
Owen Jones <jones@biology.sdu.dk>
2021-02-23 13:35:46 +0100

    Modified all tests with `expect_is` (deprecated)
    Replaced expect_is with expect_type, expect_s3_class and expect_true("x" %in% class(y)) etc.

--- tests/testthat/test-cdb_metadata.R
+++ tests/testthat/test-cdb_metadata.R
@@ -4,7 +4,7 @@ test_that("cdb_metadata works correctly", {
   
   Compadre_metadata <- cdb_metadata(Compadre)
   
-  expect_is(Compadre_metadata, "tbl_df")
+  expect_s3_class(Compadre_metadata, "tbl_df")
   expect_true(ncol(Compadre_metadata) == (ncol(Compadre)-1))
 })
 
