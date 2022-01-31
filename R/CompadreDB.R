9991922bbbe1cf5a717781058ffbd511760057d6
Owen Jones <jonesor@gmail.com>
2022-01-31 10:51:32 +0100

    Revert "Changed from magrittr pipes to native pipes. added dplyr:: to rename function."
    This reverts commit 06e2d8bddd3c9abead7c9b49b534e9d4a0ae11c8.

--- R/CompadreDB.R
+++ R/CompadreDB.R
@@ -164,13 +164,13 @@ as_cdb <- function(from) {
   #Modify the column names to reflect database backend updates if necessary
   #DOI.ISBN becomes DOI_ISBN
   if("DOI.ISBN" %in% names(dat)){
-    dat <- dat |> 
-      dplyr::rename(DOI_ISBN = DOI.ISBN)
+    dat <- dat %>% 
+      rename(DOI_ISBN = DOI.ISBN)
   }
   #AnnualPeriodicity becomes ProjectionInterval
   if("AnnualPeriodicity" %in% names(dat)){
-    dat <- dat |> 
-      dplyr::rename(ProjectionInterval = AnnualPeriodicity)
+    dat <- dat %>% 
+      rename(ProjectionInterval = AnnualPeriodicity)
   }
   
   
